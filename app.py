4bf7e740eb7bcf4764d9cc2b2f566cc478d13ba7
Jose Ayazo <josemanuelayazo@gmail.com>
2021-11-17 08:54:59 -0500

    flask app created

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import flask_cors import CORS
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "hello"
+
+
+@app.route("/predict")
+def predict():
+    return "Cassava Green Mite"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
